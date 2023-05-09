1db27ff9fb1cc6cefca3f159cf9d50492466e657
mdg0410 <levoyersteven@gmail.com>
2023-05-09 17:23:48 -0500

    Add files for code review

--- src/modules/task.js
+++ src/modules/task.js
@@ -13,12 +13,11 @@ export default class Task {
   createNode() {
     const taskNode = document.createElement('li');
     taskNode.setAttribute('data-task', true);
-    taskNode.setAttribute('readonly', true);
     taskNode.innerHTML = `
     <div class="task-item-container">
       <label htmlFor="complete-task" class="task-status"><input ${this.completed ? 'checked' : ''} type="checkbox" name="complete-task"/>${!this.completed ? `<i class="bi bi-square" data-task-id="${this.index}"></i>` : `<i class="bi bi-check-lg" data-task-id="${this.index}"></i>`} </label>
       <form name="task-${this.index}" autocomplete="off">
-        <input type="text" class="task-item ${this.completed ? 'task-completed' : ''}" required name="description" data-task-id="${this.index}" value="${this.description}" readonly/>
+        <input type="text" class="task-item ${this.completed ? 'task-completed' : ''}" required name="description" data-task-id="${this.index}" value="${this.description}" />
         <input type="hidden" value="${this.index}" name="index" />
       </form>
     </div>
